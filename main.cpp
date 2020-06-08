0bceaf8cda5d1ae5d553c09cd46c12483c1112eb
Aleksey Smirnov <55441212+leshastern@users.noreply.github.com>
2020-06-09 00:43:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -71,11 +71,12 @@ int main()
 	cin >> speed;
         
 	Context* context = new Context();
-	if (speed == 100) context->set_strategy(new Electricity);
-	else if (speed == 200) context->set_strategy(new Gas);
-	else if (speed == 300) context->set_strategy(new Petrol);
+	if (speed == 100) context->set_strategy();
+	else if (speed == 200) context->set_strategy();
+	else if (speed == 300) context->set_strategy();
 	else cout << "Error: invalid data" << endl;
-
+        
+	context->DoSomeBusinessLogic();
 	delete context;
 
 	system("PAUSE");
